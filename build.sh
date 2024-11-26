992a116606bb663cd9aa54a77548163394988007
RobertoDiazEc <robertodiazec@gmail.com>
2024-11-25 21:39:16 -0500

    activo back api deploy 1.01

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
-reflex export --frontend-only
+API_URL=https://api.cpkm.com.co reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f  frontend.zip
