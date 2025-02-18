1cd6f2ff29eb6d6a01db8a344aadf9053a0718e4
RobertoDiazEc <robertodiazec@gmail.com>
2025-02-18 15:21:55 -0500

    error gramatical en pagina inicio, se vuleve a publicar por esta falla

--- link_bio/views/empresa/empresadet.py
+++ link_bio/views/empresa/empresadet.py
@@ -15,5 +15,5 @@ class Empresadetobjetivos(Enum):
     OBJETIVO2 = "Cumplir la legislación vigente para garantizar un entorno de trabajo seguro y confiable."
     OBJETIVO3 = "Diseñar e implementar estrategias efectivas de comunicación interna para que nuestros colaboradores ofrezcan servicios de calidad alineados a las necesidades de los clientes."
     OBJETIVO4 = "Mantener operaciones eficientes, seguras y  con soluciones de alto impacto,asegurando el cumplimiento de metas y un desarrollo continuo."
-    OBJETIVO5 = "Contas con proveedores estratégicos que garanticen el suministro de materiales e insumos en condiciones óptimas, fortaleciendo la calidad de nuestros servicios."
+    OBJETIVO5 = "Contar con proveedores estratégicos que garanticen el suministro de materiales e insumos en condiciones óptimas, fortaleciendo la calidad de nuestros servicios."
     
